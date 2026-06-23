e64ee82310cd54c2f7feb29f9964336f227a22f5
Fadil Leo Kurniawan <fadilleokurniawan19@gmail.com>
2026-06-23 16:22:13 +0700

    upgrade kode studio page

--- store/usePhotoStore.ts
+++ store/usePhotoStore.ts
@@ -1,9 +1,9 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 interface PhotoStore {
   photos: string[];
   addPhoto: (photo: string) => void;
-  updatePhoto: (index: number, newPhoto: string) => void; // Fungsi baru untuk Retake
+  updatePhoto: (index: number, photo: string) => void; // PASTIKAN BARIS INI ADA
   clearPhotos: () => void;
 }
 
@@ -11,7 +11,7 @@ export const usePhotoStore = create<PhotoStore>((set) => ({
   photos: [],
   addPhoto: (photo) => set((state) => ({ photos: [...state.photos, photo] })),
   
-  // Logika untuk menimpa foto pada index/urutan tertentu
+  // PASTIKAN FUNGSI INI DITAMBAHKAN
   updatePhoto: (index, newPhoto) => set((state) => {
     const newPhotos = [...state.photos];
     newPhotos[index] = newPhoto;
